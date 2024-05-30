5b9171a5272db85a40f714b0aa85775175691957
AditaDaita <zltr_adrian@yahoo.com>
2024-05-30 03:17:15 +0300

    Add project files.

---
+++ Tema - Ciorba de peste/Tema - Ciorba de peste.cpp
@@ -0,0 +1,24 @@
+// Tema - Ciorba de peste.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+	//Ciorba de peste Trio
+	//Pentru o ciorba avem nevoie de 3 pesti. 
+	// De cati peste avem nevoie pentru a obtine x ciorbe?
+	int a = 3, b, c;
+	std::cout << "Introduceti numarul dorit de ciorbe " << std::endl;
+	std::cin >> b;
+	c = a * b;
+	std::cout << c << std::endl;
+
+	
+
+
+
+
+}
+
+
